29f48614bf6fcc0030c18091b905b64f4fd54570
clean-code-craft <cleancodcraft@gmail.com>
2020-10-06 11:09:21 +0530

    Updated

--- Samples/DataStructureDemo/Program.cs
+++ Samples/DataStructureDemo/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace DataStructureDemo
 {
+    //Dynamic Array
+    public class List
+    {
+        private int[] buffer;
+
+        public List(int capacity)
+        {
+            if (capacity == 0)
+            {
+                capacity = 5;
+            }
+            buffer = new int[capacity];
+
+        }
+
+        private void ReSizeBuffer()
+        {
+            int[] temp = new int[buffer.Length + 10];
+            for(int i = 0; i < buffer.Length; i++)
+            {
+                temp[i] = buffer[i];
+            }
+            buffer = temp;
+
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            List _list = new List(0);
+            _list[0] = 10;
+            _list[1] = 20;
+            _list[2] = 30;
+            _list[3] = 40;
+            _list[4] = 50;
+            _list[5] = 60;
+
+            int value = _list[5];//60
+            Console.WriteLine(value);
+            Console.WriteLine("Total Number of  Elements in the List " + _list.ItemCount);
+            Console.WriteLine("List Capacity " + _list.Capacity);
+
+
+
         }
     }
 }
