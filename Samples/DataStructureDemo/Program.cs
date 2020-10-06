c13d9a3ca5f356846879c325f66f7460068da56d
clean-code-craft <cleancodcraft@gmail.com>
2020-10-06 12:37:27 +0530

    List

--- Samples/DataStructureDemo/Program.cs
+++ Samples/DataStructureDemo/Program.cs
@@ -7,10 +7,16 @@ using System.Threading.Tasks;
 namespace DataStructureDemo
 {
     //Dynamic Array
+    //Dynamic Array
+    /*
     public class List
     {
+        #region Private Data Members
         private int[] buffer;
+        private int count;
+        #endregion
 
+        #region Constructor
         public List(int capacity)
         {
             if (capacity == 0)
@@ -18,37 +24,242 @@ namespace DataStructureDemo
                 capacity = 5;
             }
             buffer = new int[capacity];
+            count = 0;
+        }
+        #endregion
 
+        #region indexer
+        public int this[int index]
+        {
+            get
+            {
+                return buffer[index];
+            }
+            set
+            {
+                if (index >= buffer.Length)
+                {
+                    Console.WriteLine("Invoke ResizeBuffer");
+                    ReSizeBuffer();
+                }
+                buffer[index] = value;
+
+                if ((index + 1) >= count)
+                {
+                    count = (index + 1);
+                }
+            }
+        }
+        #endregion
+
+        #region Properties
+        public int ItemCount
+        {
+            get
+            {
+                return count;
+            }
         }
 
+        public int Capacity
+        {
+            get
+            {
+                return buffer.Length;
+            }
+        }
+
+        #endregion
+        #region Methods
         private void ReSizeBuffer()
         {
             int[] temp = new int[buffer.Length + 10];
-            for(int i = 0; i < buffer.Length; i++)
+            for (int i = 0; i < buffer.Length; i++)
             {
                 temp[i] = buffer[i];
             }
             buffer = temp;
-
         }
+        #endregion
 
     }
+    */
+
+    /*
+    public class List
+    {
+        #region Private Data Members
+        private object[] buffer;
+        private int count;
+        #endregion
+
+        #region Constructor
+        public List(int capacity)
+        {
+            if (capacity == 0)
+            {
+                capacity = 5;
+            }
+            buffer = new object[capacity];
+            count = 0;
+        }
+        #endregion
+
+        #region indexer
+        public object this[int index]
+        {
+            get
+            {
+                return buffer[index];
+            }
+            set
+            {
+                if (index >= buffer.Length)
+                {
+                    Console.WriteLine("Invoke ResizeBuffer");
+                    ReSizeBuffer();
+                }
+                buffer[index] = value;
+
+                if ((index + 1) >= count)
+                {
+                    count = (index + 1);
+                }
+            }
+        }
+        #endregion
+
+        #region Properties
+        public int ItemCount
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return buffer.Length;
+            }
+        }
+
+        #endregion
+        #region Methods
+        private void ReSizeBuffer()
+        {
+            object[] temp = new object[buffer.Length + 10];
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                temp[i] = buffer[i];
+            }
+            buffer = temp;
+        }
+        #endregion
+
+    }
+    */
+
+    public class List<T>
+    {
+        #region Private Data Members
+        private T[] buffer;
+        private int count;
+        #endregion
+
+        #region Constructor
+        public List(int capacity)
+        {
+            if (capacity == 0)
+            {
+                capacity = 5;
+            }
+            buffer = new T[capacity];
+            count = 0;
+        }
+        #endregion
+
+        #region indexer
+        public T this[int index]
+        {
+            get
+            {
+                return buffer[index];
+            }
+            set
+            {
+                if (index >= buffer.Length)
+                {
+                    Console.WriteLine("Invoke ResizeBuffer");
+                    ReSizeBuffer();
+                }
+                buffer[index] = value;
+
+                if ((index + 1) >= count)
+                {
+                    count = (index + 1);
+                }
+            }
+        }
+        #endregion
+
+        #region Properties
+        public int ItemCount
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return buffer.Length;
+            }
+        }
+
+        #endregion
+        #region Methods
+        private void ReSizeBuffer()
+        {
+            T[] temp = new T[buffer.Length + 10];
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                temp[i] = buffer[i];
+            }
+            buffer = temp;
+        }
+        #endregion
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            List _list = new List(0);
+            List<int> _list = new List<int>(0);
             _list[0] = 10;
             _list[1] = 20;
             _list[2] = 30;
             _list[3] = 40;
             _list[4] = 50;
             _list[5] = 60;
-
-            int value = _list[5];//60
+            int value = (int)_list[6];
             Console.WriteLine(value);
             Console.WriteLine("Total Number of  Elements in the List " + _list.ItemCount);
             Console.WriteLine("List Capacity " + _list.Capacity);
+
+            List<string> _stringList = new List<string>(10);
+            _stringList[0] = "One";
+
+            List<double> _doubleList = new List<double>(20);
+            _doubleList[0] = 12.34;
+
+
 
 
 
