7ac42d0384f558fea5e3d12ab3536a20dee4c1c0
clean-code-craft <cleancodcraft@gmail.com>
2020-10-08 12:09:06 +0530

    Update Program.cs

--- Samples/EventDemo/Program.cs
+++ Samples/EventDemo/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace EventDemo
 {
+    //One who has state / EventSource
+    public class Subject
+    {
+        string state;
+        public void ChangeState(string newState)
+        {
+            this.state = newState;
+
+        }
+    }
+    class ObserverOne { 
+
+        public void Notify() {
+            Console.WriteLine("Oserver One Notified");
+        }
+    }
+    class ObserverTwo
+    {
+        public void Notify()
+        {
+            Console.WriteLine("Observer Two Notified");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Subject _subjectRef = new Subject();
+            _subjectRef.ChangeState("State1");
+            _subjectRef.ChangeState("State2");
         }
     }
 }
